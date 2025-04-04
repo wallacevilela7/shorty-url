63dbde07db4775d5d6ef730460ee5bcb611cc421
wallacevilela7 <wallace.vilela9@gmail.com>
2025-04-04 17:16:54 -0300

    Create spring app

---
+++ src/main/java/tech/wvs/shortyurl/ShortyurlApplication.java
@@ -0,0 +1,13 @@
+package tech.wvs.shortyurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortyurlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortyurlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/wvs/shortyurl/ShortyurlApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.wvs.shortyurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortyurlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
